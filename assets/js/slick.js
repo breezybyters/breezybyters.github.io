33a7a57531ab9d816b45bd31862f44c607627621
salastro <salah2112004@gmail.com>
2023-06-11 17:36:34 +0300

    js/slick: remove unnecessary responsive options

--- assets/js/slick.js
+++ assets/js/slick.js
@@ -8,13 +8,4 @@ $('.slider').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     centerMode: true,
-    responsive: [
-        {
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-        },
-    ]
 });
